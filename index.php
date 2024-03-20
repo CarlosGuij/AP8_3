863f6e7433b01e9c36f973e80a7fb9958e4ca9f5
Carlos <cagumi@floridauniversitaria.es>
2024-03-20 16:49:17 +0100

    Change a one thing for $producto

--- index.php
+++ index.php
@@ -3,18 +3,18 @@ require_once 'autoloader.php';
 
 $modelo = new Modelo();
 
-$productos = $modelo->getAllProductos("ASC");
-$modelo->showPaginator(1, 5);
+$productos = $modelo->getAllProductos();
+$modelo->showPaginator();
 
 echo "<table>";
 echo "<tr>";
-$modelo->showOrderAction();
+echo "<th>" . $modelo->showOrderAction() . "</th>";
 echo "</tr>";
 foreach ($productos as $producto) {
     echo "<tr>";
-    echo "<td>" . $producto['id'] . "</td>";
-    echo "<td>" . $producto['descripcion'] . "</td>";
-    echo "<td>" . $producto['precio'] . "</td>";
+    echo "<td>" . $producto['PROD_NUM'] . "</td>";
+    echo "<td>" . $producto['DESCRIPCION'] . "</td>";
+
     echo "</tr>";
 }
 echo "</table>";
